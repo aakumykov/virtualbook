545fa5d81b5302160942c1e3d440fee08fc14aca
Андрей Кумыков <aakumykov@yandex.ru>
2016-07-30 22:55:39 +0700

    Список текущих проблем

--- lib/spider/spider.rb
+++ lib/spider/spider.rb
@@ -19,6 +19,8 @@ class Spider
 	def initialize(&block)
 		debug_msg "#{self.class}.#{__method__}(#{block})"
 		instance_eval(&block) if block_given?
+
+		@threads ||= 1
 	end
 
 	def add_source(uri)
@@ -38,6 +40,7 @@ class Spider
 		debug_msg "#{self.class}.#{__method__}(#{uri})"
 
 		src = uri || @@source
+		src = [src] if not src.is_a? Array
 			debug_msg " src: #{src}"
 
 		threads = []
@@ -51,7 +54,7 @@ class Spider
 						debug_msg "загружена страница: #{page.class}, размер: #{page.to_s.size} байт"
 					
 					@after_load and page = @after_load.call(uri,page)
-						debug_msg "фильтрованная страница: #{page.class}, размер: #{page.size} байт"
+						debug_msg "фильтрованная страница: #{page.class}, размер: #{page.to_s.size} байт"
 				end
 			end
 		end
@@ -67,34 +70,34 @@ class Spider
 end
 
 
-Msg.info "#{'~'*15} вызов с блоком #{'~'*15}"
+# Msg.info "#{'~'*15} вызов с блоком #{'~'*15}"
 
-Spider.create do |sp|
-	sp.add_source('http://opennet.ru')
-	#sp.add_source('http://ru.wikipedia.org/wiki/FreeBSD')
+# Spider.create do |sp|
+# 	sp.add_source('http://opennet.ru')
+# 	#sp.add_source('http://ru.wikipedia.org/wiki/FreeBSD')
 	
-	sp.depth = 2
-	sp.pages_per_node = 3
+# 	sp.depth = 2
+# 	sp.pages_per_node = 3
 
-	sp.threads = 3
+# 	sp.threads = 3
 	
-	sp.before_load = lambda { |uri| 
-		sp.info 'предобработка'
-		Filter.link(uri) 
-	}
+# 	sp.before_load = lambda { |uri| 
+# 		sp.info 'предобработка'
+# 		Filter.link(uri) 
+# 	}
 
-	sp.after_load = lambda { |uri,page| 
-		sp.info 'постобработка'
-		Filter.page(uri,page) 
-	}
-end.download
+# 	sp.after_load = lambda { |uri,page| 
+# 		sp.info 'постобработка'
+# 		Filter.page(uri,page) 
+# 	}
+# end.download
 
 
-#Msg.info "#{'~'*15} вызов объектом #{'~'*15}"
+# Msg.info "#{'~'*15} вызов объектом #{'~'*15}"
 
 # sp = Spider.new
 # sp.add_source 'http://linux.org.ru'
-# sp.add_source 'lib.ru'
+# sp.add_source 'http://lib.ru'
 # sp.depth = 3
 # sp.pages_per_node = 3
 # sp.before_load = lambda { |uri| Filter.link(uri) }
@@ -102,9 +105,9 @@ end.download
 # data = sp.download
 
 
-#Msg.info "#{'~'*15} вызов объектом 2 #{'~'*15}"
+Msg.info "#{'~'*15} вызов объектом 2 #{'~'*15}"
 
-# sp2 = Spider.new
-# sp2.depth = 1
-# data = sp2.download page:'http://bash.im/comics'
-# data = sp2.download hash:'http://bash.im/comics'
+sp2 = Spider.new
+sp2.depth = 1
+data = sp2.download 'http://bash.im/comics'
+data = sp2.download 'http://geektimes.ru'
